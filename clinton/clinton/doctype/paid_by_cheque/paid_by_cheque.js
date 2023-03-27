59e85ce7e267845f1b45dc4d455603c269a9f1f4
UsamaNaveed9 <usamanaveed9263@gmail.com>
2023-03-27 08:08:38 +0000

    Paid by cheque doctype

---
+++ clinton/clinton/doctype/paid_by_cheque/paid_by_cheque.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, usama and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Paid by Cheque', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ clinton/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Clinton",
+			"type": "module",
+			"label": _("Clinton")
+		}
+	]
